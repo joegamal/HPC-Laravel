d968c16466d0ac450bfaf942f074e2f5e654ccef
joegamal <yusufmagamalth@gmail.com>
2026-05-15 02:23:53 +0300

    clean

--- withCargo/cargo_rust/target/debug/cargo_rust.d
+++
@@ -1,1 +0,0 @@
-/home/yusufgamal/projects/RustFirst/withCargo/cargo_rust/target/debug/cargo_rust: /home/yusufgamal/projects/RustFirst/withCargo/cargo_rust/src/main.rs
--- withCargo/cargo_rust/target/debug/deps/cargo_rust-496f8d9699826fdf.d
+++
@@ -1,5 +0,0 @@
-/home/yusufgamal/projects/RustFirst/withCargo/cargo_rust/target/debug/deps/cargo_rust-496f8d9699826fdf.d: src/main.rs
-
-/home/yusufgamal/projects/RustFirst/withCargo/cargo_rust/target/debug/deps/cargo_rust-496f8d9699826fdf: src/main.rs
-
-src/main.rs:
